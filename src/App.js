8bf3c505d94d48a1e8622dd497abcc5caeec286b
zivarh <zivar.hagverdiyeva@gmail.com>
2022-04-09 21:58:00 +0400

    deployed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = React.useState([
+    {
+      text: "Learn about React",
+      isCompleted: false
+    },
+    {
+      text: "Meet friend for lunch",
+      isCompleted: false
+    },
+    {
+      text: "Build really cool todo app",
+      isCompleted: false
+    }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
